a3ccbf6cfcbb2db32a788ad4e48367e5bf205d3b
William O'Beirne <wbobeirne@gmail.com>
2015-04-19 19:42:25 -0400

    Improved IPE styles.

--- themes/abc/template.php
+++ themes/abc/template.php
@@ -4,13 +4,15 @@
  * Implements template_preprocess_html().
  *
  */
-//function abc_preprocess_html(&$variables) {
-//  // Add conditional CSS for IE. To use uncomment below and add IE css file
-//  drupal_add_css(path_to_theme() . '/css/ie.css', array('weight' => CSS_THEME, 'browsers' => array('!IE' => FALSE), 'preprocess' => FALSE));
-//
-//  // Need legacy support for IE downgrade to Foundation 2 or use JS file below
-//  // drupal_add_js('http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js', 'external');
-//}
+function abc_preprocess_html(&$variables) {
+  // Admins get special CSS
+  if (user_access('view the administration theme')) {
+    drupal_add_css(path_to_theme() . '/css/admin.css');
+  }
+
+  // Need legacy support for IE downgrade to Foundation 2 or use JS file below
+  // drupal_add_js('http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js', 'external');
+}
 
 /**
  * Implements template_preprocess_page
--- themes/abc/templates/fieldable-panels-panes--homepage-block-node.tpl.php
+++ themes/abc/templates/fieldable-panels-panes--homepage-block-node.tpl.php
@@ -1,3 +1,4 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $images; ?>
+  <?php dpm($variables); ?>
 </div>
